99ee42e32d0b0d76f22b2b3068f11e6bbcc57145
Ashish Sinsinwal <ashishsinsinwal@outlook.com>
2025-08-07 12:24:51 +0530

    added Cloudinary file upload integration

--- backend/routes/pg.routes.js
+++ backend/routes/pg.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Pg } from '../config/db.js';
-
+const {storage} = require('../cloudConfig.js');
+const multer = require('multer');
+const upload = multer({storage});
 const router = express.Router();
 
 
